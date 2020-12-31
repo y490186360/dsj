9211ddc7811c743c672523987fe18f39bb484bce
Yuki <490186360@qq.com>
2020-12-31 13:57:02 +0800

    测试

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function () {
+    // 调用 getUserInfo 获取用户基本信息
+    getUserInfo()
+    
+}) 
+var layer = layui.layer
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user){
+    // 优先 nickname
+    // 1.获取用户名称
+    var name = user.nickname || user.username
+    // 2.设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null){
+        // 3.1 渲染图片头像
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else {
+        // 3.2 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar')
+          .html(first)
+          .show()
+      }
+}
